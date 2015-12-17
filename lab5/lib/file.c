782d3fffe2b4e5b461686829ce8ed84b8cb5d024
stone <wy30123@163.com>
2015-12-17 00:58:14 -0500

    server read/write finished

--- lab5/lib/file.c
+++ lab5/lib/file.c
@@ -100,7 +100,16 @@ devfile_read(struct Fd *fd, void *buf, size_t n)
 	// bytes read will be written back to fsipcbuf by the file
 	// system server.
 	// LAB 5: Your code here
-	panic("devfile_read not implemented");
+	//panic("devfile_read not implemented");
+	/*stone's solution for lab5*/
+	int r;
+	fsipcbuf.read.req_fileid = fd->fd_file.id;
+	fsipcbuf.read.req_n = n;
+	if ((r = fsipc(FSREQ_READ, NULL)) < 0)
+		return r;
+	memmove(buf, &fsipcbuf, r);
+	return r;
+	
 }
 
 // Write at most 'n' bytes from 'buf' to 'fd' at the current seek position.
@@ -116,7 +125,23 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	// remember that write is always allowed to write *fewer*
 	// bytes than requested.
 	// LAB 5: Your code here
-	panic("devfile_write not implemented");
+	//panic("devfile_write not implemented");
+	/*stone's solution for lab5*/
+	int r;
+	void* current;
+	ssize_t ret;
+	fsipcbuf.write.req_fileid = fd->fd_file.id;
+	current = (void*)buf;
+	while (n) {
+		fsipcbuf.write.req_n = MIN(n, sizeof(fsipcbuf.write.req_buf));
+		memmove(fsipcbuf.write.req_buf, buf, fsipcbuf.write.req_n);
+		if ((r = fsipc(FSREQ_WRITE, NULL)) < 0)
+			return r;
+		n -= r;
+		current += r;
+	}
+	ret = (ssize_t)(current - buf);
+	return ret;
 }
 
 static int
