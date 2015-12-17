33ec521c2de761cbae8934f735eb580c9a854e4b
stone <wy30123@163.com>
2015-12-16 20:53:32 -0500

    block alloc finished

--- lab5/fs/fs.c
+++ lab5/fs/fs.c
@@ -61,7 +61,16 @@ alloc_block(void)
 	// super->s_nblocks blocks in the disk altogether.
 
 	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	//panic("alloc_block not implemented");
+	/*stone's solution for lab5*/
+	uint32_t blockno;
+	for (blockno = 2; blockno < super->s_nblocks; blockno++){
+		if (block_is_free(blockno)){
+			bitmap[blockno/32] &= ~(1<<(blockno%32));
+			flush_block(bitmap);
+			return blockno;
+		}
+	}
 	return -E_NO_DISK;
 }
 
