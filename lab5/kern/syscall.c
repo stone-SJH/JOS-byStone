ab32cbec382b8ab17f2decb9b8c89b4a370beec2
stone <wy30123@163.com>
2016-01-16 23:20:52 -0500

    spawning processes finished

--- lab5/kern/syscall.c
+++ lab5/kern/syscall.c
@@ -154,7 +154,16 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	//panic("sys_env_set_trapframe not implemented");
+	/*stone's solution for lab5*/
+	int r;
+	struct Env* e;
+	if ((r = envid2env(envid, &e, 1)) < 0)
+		return -E_BAD_ENV;
+	e->env_tf = *tf;
+	e->env_tf.tf_cs |= 3;
+	e->env_tf.tf_eflags |= FL_IF;
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -513,6 +522,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			break;
 		case SYS_ipc_try_send:
 			ret = sys_ipc_try_send((envid_t)a1, a2, (void*)a3, (int)a4);
+			break;
+		case SYS_env_set_trapframe:
+			ret = sys_env_set_trapframe((envid_t)a1, (struct Trapframe*)a2);
 			break;
 		default:
 			break;
